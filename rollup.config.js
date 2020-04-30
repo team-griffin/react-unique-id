341bfc54e61dcd6b3d5d72349fd9304ca7905b65
Jack Ellis <jack.ellis@godaddy.com>
2020-04-30 15:17:40 +0100

    fix: only generate a single dist file

--- rollup.config.js
+++ rollup.config.js
@@ -1,29 +1,23 @@
 import babel from 'rollup-plugin-babel';
 import localResolve from 'rollup-plugin-local-resolve';
-import replace from 'rollup-plugin-replace';
-import uglify from 'rollup-plugin-uglify-es';
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/es/react-unique-id.production.js',
+      file: 'dist/es/react-unique-id.js',
       format: 'es',
     },
     {
-      file: 'dist/cjs/react-unique-id.production.js',
+      file: 'dist/cjs/react-unique-id.js',
       format: 'cjs',
     },
   ],
   plugins: [
     localResolve(),
-    replace({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
     babel({
       exclude: 'node_modules/**',
       plugins: ['external-helpers'],
     }),
-    uglify(),
   ],
 };
